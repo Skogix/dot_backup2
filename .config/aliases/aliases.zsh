da474751b277a0b2b2df5c5900e6a9edf1d3fc16
Emil Skogsund <emil.skogsund@gmail.com>
2024-06-08 12:05:25 +0200

    .

---
+++ .config/aliases/aliases.zsh
@@ -0,0 +1,15 @@
+alias l='lsd'
+alias lsa='lsd -a'
+alias vimrc='nvim ~/.config/nvim/init.lua'
+alias aliasrc='nvim ~/.config/aliases/aliases.zsh'
+alias vimrc="nvim ~/.config/nvim/init.lua"
+alias zshrc="nvim ~/.zshrc"
+alias myip="curl http://ipecho.net/plain; echo"
+
+alias ljud="systemctl --user restart wireplumber pipewire pipewire-pulse"
+alias dockerpurge="docker system prune -a --volumes"
+alias dlog="docker compose -f /home/skogix/srv/docker-compose.yaml logs"
+alias dtail="docker compose -f /home/skogix/srv/docker-compose.yaml logs -tf --tail="50" crowdsec"
+alias dup="docker compose -f /home/skogix/srv/docker-compose.yaml up -d"
+alias dprune="docker system prune -a --volumes"
+alias dtop="oxker"
