44a7e7466b7a66f979e8d9b694ab125c2dac8781
= <=>
2024-06-11 08:44:09 +0200

    .

--- .config/aliases/aliases.zsh
+++ .config/aliases/aliases.zsh
@@ -4,16 +4,20 @@ alias vimrc='nvim ~/.config/nvim/init.lua'
 alias envrc='nvim ~/.env'
 alias aliasrc='nvim ~/.config/aliases/aliases.zsh'
 alias bootstraprc='nvim ~/.config/yadm/bootstrap'
+alias dockerrc='nvim ~/srv/docker-compose.yml'
 alias vimrc="nvim ~/.config/nvim/init.lua"
 alias zshrc="nvim ~/.zshrc"
+
 alias myip="curl http://ipecho.net/plain; echo"
+alias getports='sudo lsof -i -P -n | grep LISTEN'
 
 alias ljud="systemctl --user restart wireplumber pipewire pipewire-pulse"
 alias dockerpurge="docker system prune -a --volumes"
-alias dlog="docker compose -f /home/skogix/srv/docker-compose.yaml logs"
-alias dtail="docker compose -f /home/skogix/srv/docker-compose.yaml logs -tf --tail="50" crowdsec"
-alias dup="docker compose -f /home/skogix/srv/docker-compose.yaml up -d"
-alias ddown='docker stop $(docker ps -a -q)'
+alias dlog="docker compose -f /home/skogix/srv/docker-compose.yml logs"
+alias dtail="docker compose -f /home/skogix/srv/docker-compose.yml logs -tf --tail="50" crowdsec"
+alias dup="docker compose -f /home/skogix/srv/docker-compose.yml up -d"
+alias ddown="docker compose -f /home/skogix/srv/docker-compose.yml down"
+alias ddownall='docker stop $(docker ps -a -q)'
 alias dprune="docker system prune -a --volumes"
 alias dtop="oxker"
 alias aliases="als"
